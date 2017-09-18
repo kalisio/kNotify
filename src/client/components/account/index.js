70aaf346c6042e02efb120aa0ea6c6f56dfa3a40
Luc Claustres <luc.claustres@orange.fr>
2017-09-18 23:09:51 +0200

    Fixed missing components in index.js

--- src/client/components/account/index.js
+++ src/client/components/account/index.js
@@ -1,7 +1,11 @@
 import KResetPassword from './KResetPassword.vue'
 import KVerifySignup from './KVerifySignup.vue'
+import KSendResetPassword from './KSendResetPassword.vue'
+import KResendVerifySignup from './KResendVerifySignup.vue'
 
 export {
   KResetPassword,
-  KVerifySignup
+  KVerifySignup,
+  KSendResetPassword,
+  KResendVerifySignup
 }
