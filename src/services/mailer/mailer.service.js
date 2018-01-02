1c2087d78c31b401f44941268401215cf7ab1c1a
Luc Claustres <luc.claustres@orange.fr>
2018-01-02 23:39:46 +0100

    Added debug log

--- src/services/mailer/mailer.service.js
+++ src/services/mailer/mailer.service.js
@@ -1,8 +1,12 @@
 import mailer from 'feathers-mailer'
 import smtpTransport from 'nodemailer-smtp-transport'
+import makeDebug from 'debug'
+
+const debug = makeDebug('kalisio:kNotify:mailer')
 
 export default function (name, app, options) {
   // Keep track of config
   Object.assign(options, app.get('mailer'))
-  return mailer(smtpTransport(options))
+  debug('Mailer created with config ', options)
+   return mailer(smtpTransport(options))
 }
