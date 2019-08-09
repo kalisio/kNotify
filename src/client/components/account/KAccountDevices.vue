fcd63650284ee27fd29d9e90ad46a9019d6e955d
Christophe Nouguier <christophe.nouguier@free.fr>
2019-08-09 18:53:22 +0200

    wip: https://github.com/kalisio/kdk/issues/31 (migrated KSignupAlert/cleaned KAccountDevices)

--- src/client/components/account/KAccountDevices.vue
+++ src/client/components/account/KAccountDevices.vue
@@ -1,44 +1,35 @@
 <template>
-  <!-- 
-    Devices collection
-  -->
-  <div class="content">
-    <div class="column items-center">
-      <q-icon size="2rem" name="error_outline" class="float-left"/>
-      {{$t('KAccountDevices.DEVICES_USED')}}<br /><br />
-    </div>
-    <div v-if="items.length > 0" class="column justify-center">
-      <div class="row">
-        <k-device-card v-for="item in items" :id="item.uuid" :key="item.uuid" :item="item" />
+  <div class="column">
+    <div class="row justify-center text-center">
+      <div class="q-pa-md">
+        <q-icon size="2rem" name="mobile_friendly" />
+        <p>{{$t('KAccountDevices.DEVICES_USED')}}</p>
       </div>
     </div>
-    <div v-else class="column items-center">
-      <div>
-        <q-icon size="2rem" name="error_outline" />
-      </div>
-      <div class="message">
-        {{$t('KGrid.EMPTY_GRID')}}
+    <div v-if="items.length > 0" class="row">
+      <template v-for="item in items">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2" :key="item.uuid">
+          <k-device-card class="q-pa-sm" :id="item.uuid" :item="item" />
+        </div>
+      </template>
+    </div>
+    <div v-else class="row justify-center text-center">
+      <div class="q-ma-xl">
+        <q-icon size="3rem" name="error_outline" />
+        <p>{{$t('KGrid.EMPTY_GRID')}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { QIcon } from 'quasar'
 import { mixins as coreMixins } from '@kalisio/kdk-core/client'
 
 export default {
   name: 'k-account-devices',
-  components: {
-    QIcon
-  },
-  data () {
-    return {
-    }
-  },
-  computed: {
-  },
-  mixins: [coreMixins.baseCollection],
+  mixins: [
+    coreMixins.baseCollection
+  ],
   methods: {
     refreshCollection () {
       this.items = this.$store.get('user.devices', [])
@@ -55,9 +46,6 @@ export default {
   },
   beforeDestroy () {
     this.$events.$off('user-changed', this.refreshCollection)
-  },
-  mounted () {
-
   }
 }
 </script>
--- src/client/components/account/KSignupAlert.vue
+++ src/client/components/account/KSignupAlert.vue
@@ -1,20 +1,31 @@
 <template>
-  <q-banner
-    rounded 
-    class="bg-warning text-white"
-    v-if="!isVerified"
-  >
-    {{$t('KSignupAlert.TITLE')}}
-    <q-collapsible icon="explore" :label="$t('KSignupAlert.HELP_LABEL')">
-      <p v-html="$t('KSignupAlert.HELP_TEXT')" />
-    </q-collapsible>
-    <q-collapsible icon="explore" :label="$t('KSignupAlert.EMAIL_LABEL')">
-      <p v-html="$t('KSignupAlert.EMAIL_TEXT')" />
-    </q-collapsible>
-    <template v-slot:action>
-      <q-btn flat color="white" label="$t('KSignupAlert.ACTION')" @click="resendVerifySignup(email)" />
-    </template>
-  </q-banner>
+  <div>
+    <q-banner
+      rounded 
+      class="bg-warning text-black fixed"
+      style="top: 18px"
+      v-if="!isVerified"
+    >
+      {{$t('KSignupAlert.TITLE')}}
+      <q-expansion-item icon="explore" :label="$t('KSignupAlert.HELP_LABEL')">
+        <q-card class="bg-warning">
+          <q-card-section>
+            <p v-html="$t('KSignupAlert.HELP_TEXT')" />
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+      <q-expansion-item icon="explore" :label="$t('KSignupAlert.EMAIL_LABEL')">
+        <q-card class="bg-warning">
+          <q-card-section>
+            <p v-html="$t('KSignupAlert.EMAIL_TEXT')" />
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+      <template v-slot:action>
+        <q-btn flat color="black" :label="$t('KSignupAlert.ACTION')" @click="resendVerifySignup(email)" />
+      </template>
+    </q-banner>
+  </div>
 </template>
 
 <script>
